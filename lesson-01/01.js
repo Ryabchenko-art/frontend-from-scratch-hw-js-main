23da4180c9547a99174cc7c29ddfcd6254b9623b
Ryabchenko-art <erabcenko4@gmail.com>
2025-03-01 14:22:10 +0300

    one_js

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,9 +2,8 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
-    sum = 1;
+let sum= 1;
     while (sum <= 20) {
         sum++
-        console.log(sum)
+        console.log(sum + "0")
     }
