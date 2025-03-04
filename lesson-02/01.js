dc2f2f206a050311ab19dd74c063e49e457be23d
Ryabchenko-art <erabcenko4@gmail.com>
2025-03-04 11:49:34 +0300

    two_les

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,18 +1,28 @@
 /*
-* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу
+* на основе его роли и наличия разрешений.
 * Полученное булево значение сохраните в переменной isAccess.
 
 * Условия доступа:
-* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
-* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем
+* (isVerifiedUser)
+* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission)
+* или временным пропуском (hasTemporaryPass)
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
+const isAdmin = true
 const isVerifiedUser = true
 const hasSpecialPermission = true
-const hasTemporaryPass = false
+const hasTemporaryPass = true
 
-let isAccess
+let isAccess = true
+
+        if ((isAccess === isAdmin && hasSpecialPermission)||(isAccess === isVerifiedUser && hasTemporaryPass)) {
+            alert("Congratulations! Do you have access")
+        } else {
+            alert("You don't have access")
+    }
 
 // your code
+
