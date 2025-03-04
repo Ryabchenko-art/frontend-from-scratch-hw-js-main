1accd52d8714eaddf8a34d12e8b5e4bd98b2e226
Ryabchenko-art <erabcenko4@gmail.com>
2025-03-04 17:02:53 +0300

    two_les

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -16,7 +16,7 @@ const isVerifiedUser = false
 const hasSpecialPermission = false
 const hasTemporaryPass = false
 
-let isAccess = (isAdmin || hasSpecialPermission) && (isVerifiedUser || hasTemporaryPass)
+let isAccess = (!isAdmin || !hasSpecialPermission) && (isVerifiedUser || hasTemporaryPass)
 
 console.log(isAccess)
 
