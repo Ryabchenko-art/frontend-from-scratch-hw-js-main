1e470d6b9ec34b44595e8478e41a1ff8aef09aa9
Ryabchenko-art <erabcenko4@gmail.com>
2025-03-09 12:32:58 +0300

    three_les

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,6 +1,6 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(a, b, c) {
-    return (a >= b || a >= c) ? true : (b >= a || b >= c) ? true : (c >= a || c >= b);
+    return (a > b || a > c) ? true : (b > a || b > c) ? true : (c > a || c > b) ? true : false;
 }
-
+findLargest(1, 2, 3)
