454c034d6dfba838435b26ea23c86e4dfb5b224a
Ryabchenko-art <erabcenko4@gmail.com>
2025-04-12 15:49:25 +0300

    hw

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -1,14 +1,18 @@
 /*
- * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+ * Напишите функцию isNumeric, которая принимает строку и проверяет,
+ * представляет ли эта строка корректное числовое значение.
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
 function isNumeric(str) {
-  // your code
+  return str.trim() !== '' && !isNaN(parseFloat(str)) && isFinite(str);
 }
-
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+/*
+1. str.trim() !== '' проверяет, что строка не пустая после удаления пробелов.
+2. !isNaN(parseFloat(str)) проверяет, что значение можно преобразовать в число, и одновременно гарантирует, что значение не является NaN.
+3. isFinite(str) проверяет, является ли значение конечным числом (исключает Infinity и NaN). */
+console.log(isNumeric("123")) // Ожидаемый результат: true
+console.log(isNumeric("12.3")) // Ожидаемый результат: true
+console.log(isNumeric("123abc")) // Ожидаемый результат: false
+console.log(isNumeric("abc")) // Ожидаемый результат: false
+console.log(isNumeric(" ")) // Ожидаемый результат: false
